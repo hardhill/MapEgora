f84a9973a344869231ed3eced47d0ae92f0ad3c2
hardhill <hardhill@mail.ru>
2017-04-28 18:31:20 +0800

    изменения в структуре папок

---
+++ MapEgora/Models/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MapEgora.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        [Required()]
+        public DateTime Photocreated { get; set; }
+        public string Description { get; set; }
+        [Required()]
+        public string PhotoName { get; set; }
+
+        public int RouteId { get; set; }
+        public virtual Route Route { get; set; }
+    }
+}
--- MapEgora/Models/Route.cs
+++ MapEgora/Models/Route.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace MapEgora.Models
     public class Route
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Название маршрута является обязательным значением!")]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage ="Необходимо указать файл карты маршрута")]
         public string RouteImage { get; set; }
+        [Required(ErrorMessage = "Необходимо указать файл KML маршрута")]
+        public string RouteKML { get; set; }
+        public virtual ICollection<Photo> Photos { get; set; }
+       
     }
 }
